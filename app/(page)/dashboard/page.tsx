4e5a13de8521c3edd16ff0d0bff7ef9865e77894
LENOVO Slim 3 <bagusfernandez64@gmail.com>
2026-06-06 00:33:40 +0700

    add side bar

---
+++ app/(page)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export const dashbooardpages = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default dashbooardpages
---
+++ app/(page)/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Poppins, Geist } from "next/font/google";
+import "../globals.css";
+import { cn } from "@/lib/utils";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+
+const PoppinsFont = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "h-full",
+        "antialiased",
+        PoppinsFont.variable,
+        "font-sans",
+        geist.variable,
+      )}
+    >
+      <body className="min-h-full flex flex-col">
+        <SidebarProvider>
+          <AppSidebar />
+          <main>{children}</main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
